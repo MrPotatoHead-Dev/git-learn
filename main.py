761c556270cf40fd12fb0a001ff2347f7956170d
MrPotatoHead-Dev <jadendux@gmail.com>
2026-03-20 11:11:37 -0300

    last improvement

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def main():
     print("Hello from testing-git!")
 
-def _testing_git():
-    pass
+def multiples_of_four(i:int):
+    return {i}*4
 
 if __name__ == "__main__":
     main()
+    i = 4
+    results = multiples_of_four(i)
